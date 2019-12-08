985a2a5d8ad25a5d479662c19d166f36177a505a
Nazzareno Giannelli <nazzareno.giannelli@gmail.com>
2019-12-08 15:23:50 +0100

    Update Batch Zip Extractor.ps1

--- Batch Zip Extractor.ps1
+++ Batch Zip Extractor.ps1
@@ -8,12 +8,31 @@ Write-Host '                                         '
 Write-Host '-----------------------------------------'
 Write-Host '                                         '
 
-$FilesFolder = Read-Host -Prompt 'In which folder the zip files are located?';
+do {
+    $FilesFolder = Read-Host -Prompt 'In which folder the zip files are located?';
 
-$FilesList = Get-ChildItem -Path $FilesFolder | where {$_.extension -eq '.zip'};
+    $FilesList = Get-ChildItem -Path $FilesFolder | Where-Object {$_.extension -eq '.zip'};
 
-foreach ($ListedFile in $FilesList) {
-    mkdir -Path ('{0}\{1}' -f $ListedFile.Directory, $ListedFile.BaseName);
-    $ArgumentList = 'x "{0}" -o"{1}\{2}\"' -f $ListedFile.FullName, $ListedFile.Directory, $ListedFile.BaseName;
-    Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList $ArgumentList -Wait;
-}
+    foreach ($ListedFile in $FilesList) {
+        mkdir -Path ('{0}\{1}' -f $ListedFile.Directory, $ListedFile.BaseName);
+        $ArgumentList = 'x "{0}" -o"{1}\{2}\"' -f $ListedFile.FullName, $ListedFile.Directory, $ListedFile.BaseName;
+        Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList $ArgumentList -Wait;
+    }
+    
+    Write-Host '                                         '
+    Write-Host '-----------------------------------------'
+    Write-Host '                                         '
+    Write-Host "1 - Repeat the operation in another folder"
+    Write-Host "2 - Exit the program"
+    $choice = Read-Host "Choose a number to proceed. Press Enter to confirm."
+
+    switch ($choice) {
+        1 {
+            Write-Host "Let's process other files!"
+        }
+        2 {
+            Write-Host "Turning off...see you soon! :)"
+            Start-Sleep 3
+        }
+    }
+} until ($choice -eq 2)
