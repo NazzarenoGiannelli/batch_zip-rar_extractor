69ca41a4051ca94f32763ad2c6cac4d227241da7
Nazzareno Giannelli <nazzareno.giannelli@gmail.com>
2019-12-10 17:37:35 +0100

    Update Run Batch_Zip-Rar_Extractor.bat

--- Run Batch_Zip-Rar_Extractor.bat
+++ Run Batch_Zip-Rar_Extractor.bat
@@ -1,1 +1,1 @@
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0\Batch Zip Extractor.ps1""' -Verb RunAs}"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0\Batch_Zip-Rar_Extractor.ps1""' -Verb RunAs}"
